81173a103ddf1df1bba87bde19ad775e899353cf
6xetny <b4sti4n321@gmail.com>
2025-07-08 17:58:43 -0400

    Add files via upload

--- manejo_notas/principal.py
+++ manejo_notas/principal.py
@@ -1,48 +1,54 @@
-from data.asignaturas import asignaturas
-from data.crear_data import crear_data
-import os
+from data.conexion import ejecutar_consulta
+from data.scripts.scripts_menu import script_menu_asignaturas,script_menu_principal
+from prettytable import PrettyTable
+from auxiliares.mensajes import salir,nombre_aplicacion,volver,invalido
+from auxiliares.version import version_actual
+from negocio.negocio_asignaturas import mostrar_listado_asignaturas
 
-# READ
-def mostrar_listado_asignaturas():
+def menu_asignaturas():
+    while True:
+        opciones_asignaturas = ejecutar_consulta(script_menu_asignaturas)
+        tabla_menu_asignatura = PrettyTable()
+        tabla_menu_asignatura.field_names = ['N°','Opción']
+        for opcion_asignatura in opciones_asignaturas:
+            tabla_menu_asignatura.add_row(opcion_asignatura) # type: ignore
+        print(tabla_menu_asignatura)
+        opcion_asignatura_usuario = input(f'Seleccione su opción [0-{len(opciones_asignaturas)-1}] :')
+
+        if opcion_asignatura_usuario == '1':
+            mostrar_listado_asignaturas()
+        elif opcion_asignatura_usuario == '2':
+            pass
+        elif opcion_asignatura_usuario == '3':
+            pass
+        elif opcion_asignatura_usuario == '4':
+            pass
+        elif opcion_asignatura_usuario == '0':
+            print(volver)
+            break
+        else:
+            print(invalido)
+
+def menu_principal():
     print()
-    print('Listado de Asignaturas')
-    print('======================')
-    contador = 0
-    for asignatura in sorted(asignaturas):
-        contador += 1
-        print(f'{contador}.- {asignatura}')
+    print(f'{nombre_aplicacion} v.{version_actual}')
+    while True:
+        opciones_menu = ejecutar_consulta(script_menu_principal)
+        tabla_menu = PrettyTable()
+        tabla_menu.field_names = ['N°','Opción']
+        for asignatura in opciones_menu:
+            tabla_menu.add_row(asignatura) # type: ignore
+        print(tabla_menu)
+        opcion_usuario = input(f'Seleccione su opción [0-{len(opciones_menu)-1}] :')
 
-# READ
-def buscar_asignatura():
-    busqueda = input('Ingrese asignatura a buscar: ')
-    for asignatura in asignaturas:
-        if busqueda.lower() in asignatura.lower():
-            return asignatura
+        if opcion_usuario == '1':
+            menu_asignaturas()
+        elif opcion_usuario == '2':
+            pass
+        elif opcion_usuario == '0':
+            print(salir)
+            break
+        else:
+            print(invalido)
 
-def indice_asignatura(busqueda):
-    for i in range(len(asignaturas)):
-        if busqueda.lower() in asignaturas[i].lower():
-            return i
-
-# CREATE
-def agregar_asignatura():
-    mostrar_listado_asignaturas()
-    nueva_asignatura = input('Ingrese nueva asignatura: ')
-    asignaturas.append(nueva_asignatura.title())
-
-    crear_data('asignaturas.py','asignaturas',asignaturas)
-
-    mostrar_listado_asignaturas()
-
-# UPDATE  
-def actualizar_asignatura():
-    mostrar_listado_asignaturas()
-    busqueda = input('Ingrese asignatura a buscar: ')
-    indice = indice_asignatura(busqueda)
-    # nuevo_dato = input(f'Ingrese nuevo nombre para asignatura {asignaturas[indice]}: ')
-    # asignaturas[indice] = nuevo_dato
-
-    crear_data('asignaturas.py','asignaturas',asignaturas)
-    mostrar_listado_asignaturas()
-
-actualizar_asignatura()
+menu_principal()
